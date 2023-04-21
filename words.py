3c76144856ba314865f405a7eef0b45916375e9b
Jonkess <jonkess92@gmail.com>
2023-04-21 14:12:00 -0500

    changed category to str to avoid type error

---
+++ words.py
@@ -0,0 +1,5 @@
+super_heroes = ["Superman", "Batman", "Spiderman", "Ironman", "Wonder Woman"]
+theme_parks = ["Disneyland", "Universal Studios", "Six Flags", "SeaWorld", "Legoland"]
+ghost_types = ["Poltergeist", "Apparition", "Demon", "Wraith", "Specter"]
+
+word_list = [super_heroes, theme_parks, ghost_types]
